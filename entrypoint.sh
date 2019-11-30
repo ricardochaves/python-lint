c0ca5a9e32cc10a128764c563f14d591791dd6a6
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2019-11-30 19:27:55 -0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/sh -l
 
-echo "Use pylint: $2"
-echo "Python root: $1"
-
 if [ "$2" = true ] ; then
 
     pylint $1
     exit_code=$?
-    echo "*******"
-    echo "exit_code"
-    echo $exit_code
-    echo "*******"
+
     if [ "$exit_code" = "0" ]; then
         echo ::log-command ::"Pylint ok"
     else
@@ -19,7 +13,3 @@ if [ "$2" = true ] ; then
     fi
 
 fi
-
-# time=$(date)
-# echo ::set-output name=time::$time
-
