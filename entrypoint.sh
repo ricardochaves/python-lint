9e7b67ca8c1a8a3aed43da2237e8fe88891a3d79
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2019-11-30 17:44:29 -0300

    update sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh -l
 
-python pylint 
+ 
 
 echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
 
+if [ "$1" = true ]
+  pylint; test_result=$?
+  if ((test_result != 0)); then
+    echo '%s\n' "Test case x failed" >&2  # write error message to stderr
+    exit $test_result                     # or exit 1
+  fi
+fi
+
+# time=$(date)
+# echo ::set-output name=time::$time
+
+echo ::log-command ::"Pylint ok"
